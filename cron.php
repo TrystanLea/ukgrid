36be2e582441f98fb394bb882ec9243cce602652
Ben Aylott <ben@1d2f0.info>
2017-03-30 16:25:54 +0100

    update url for new api

--- cron.php
+++ cron.php
@@ -1,6 +1,7 @@
 <?php
 
-  $url = "http://www.bmreports.com/bsp/additional/soapfunctions.php?element=generationbyfueltypetable";
+  #See: https://www.elexonportal.co.uk/SCRIPTING
+  $url = "https://downloads.elexonportal.co.uk/fuel/download/latest?key=ELEXON-BMRS-API-KEY";
 
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL,$url);
@@ -11,8 +12,8 @@
   $xml = simplexml_load_string($output);
 
   $len = count($xml->INST->FUEL);
-  
-  
+
+
   $intensity = array(
     "CCGT"=>360,
     "OCGT"=>480,
@@ -28,30 +29,30 @@
     "INTNED"=>550,
     "INTEW"=>450
   );
-  
+
   $co2intensity = 0;
   $totalsupply = 0;
-  
+
   $data = array();
   $values = array();
-    
+
   for ($i=0; $i<$len; $i++) {
     $name = $xml->INST->FUEL[$i]['TYPE']."";
     $val = $xml->INST->FUEL[$i]['VAL']*1.0;
     $pct = $xml->INST->FUEL[$i]['PCT']*0.01;
-    
+
     $co2intensity += $pct*$intensity[$name];
-    
+
     $data["".$name."_val"] = $val;
     $data["".$name."_prc"] = $pct;
     $values[] = $val;
     $totalsupply += $val;
   }
-  
+
   $data["gridintensity"] = $co2intensity / 0.93;
   $data["totalsupply"] = $totalsupply;
   $values[] = $data["gridintensity"];
   $values[] = $data["totalsupply"];
-  
+
   file_get_contents("http://localhost/ukgrid/api.php?q=post&id=1&apikey=MANUAL-WRITE-APIKEY&values=".implode(",",$values));
 
